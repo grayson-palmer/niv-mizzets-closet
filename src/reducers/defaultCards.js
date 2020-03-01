cf15eb1f09d74381b6da977d6181b14e4dd5beb3
Grayson Palmer <gpalmer1385@gmail.com>
2020-03-01 14:38:27 -0700

    Delete unnecessary reducer file

--- src/reducers/defaultCards.js
+++
@@ -1,8 +0,0 @@
-export const defaultCardsReducer = (state = [], action) => {
-  switch (action.type) {
-    case 'DEFAULT_CARDS':
-      return action.cards
-    default:
-      return state
-  }
-}
