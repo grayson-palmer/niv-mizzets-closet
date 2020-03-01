5ee3e01e18c0ceeb0d44ac53ae4340bcbd3f99b1
Grayson Palmer <gpalmer1385@gmail.com>
2020-03-01 14:35:37 -0700

    Create fetch by card ID

--- src/apiCalls/index.js
+++ src/apiCalls/index.js
@@ -1,5 +1,5 @@
 export const fetchDefaultCards = async () => {
-  return await fetch(`https://api.scryfall.com/cards/search?order=released&q=set:thb`)
+  return await fetch(`https://api.scryfall.com/cards/search?order=released&q=set:thb+-st:token`)
   .then(res => {
     if(!res.ok) {
       throw Error('Failed to retrieve default cards.')
@@ -8,7 +8,16 @@ export const fetchDefaultCards = async () => {
 }
 
 export const fetchCardsWithSearch = async (searchInput) => {
-  return await fetch(`https://api.scryfall.com/cards/search?q=${searchInput}`)
+  return await fetch(`https://api.scryfall.com/cards/search?q=${searchInput}+-st:token`)
+  .then(res => {
+    if(!res.ok) {
+      throw Error('Failed to retrieve cards.')
+    }
+    return res.json()})
+}
+
+export const fetchCardById = async (tcg_id) => {
+  return await fetch(`https://api.scryfall.com/cards/tcgplayer/${tcg_id}`)
   .then(res => {
     if(!res.ok) {
       throw Error('Failed to retrieve cards.')
