8304a1fa7da6a115935368b5dc56887b003ec6e9
Grayson Palmer <gpalmer1385@gmail.com>
2020-03-01 17:27:23 -0700

    Add favoriteCards reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,15 @@ import { loadingReducer } from './loading.js';
 import { artistCardsReducer } from './artistCards.js';
 import { selectedCardReducer } from './selectedCard.js';
 import { selectedCardInfoReducer } from './selectedCardInfo.js';
+import { addToFavoritesReducer } from './favoriteCards.js';
 
 const rootReducer = combineReducers({
   searchCards: searchCardsReducer,
   loadingStatus: loadingReducer,
   artistCards: artistCardsReducer,
   selectedCard: selectedCardReducer,
-  selectedCardInfo: selectedCardInfoReducer
+  selectedCardInfo: selectedCardInfoReducer,
+  favoriteCards: addToFavoritesReducer
 })
 
 export default rootReducer;
